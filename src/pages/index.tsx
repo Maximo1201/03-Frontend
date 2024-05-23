c235922f58636efb7bd7ad9872d689d083d7c3f0
santiago <saforero88@gmail.com>
2024-05-22 19:58:18 -0500

    refactor: Remove unnecessary code from Home and index pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
-import Button from '@mui/material/Button';
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
+import AppAppBar from "../views/AppAppBar";
+import AppFooter from "../views/AppFooter";
+import ProductCategories from "../views/ProductCategories";
+import ProductCTA from "../views/ProductCTA";
+import ProductHero from "../views/ProductHero";
+import ProductHowItWorks from "../views/ProductHowItWorks";
+import ProductSmokingHero from "../views/ProductSmokingHero";
+import ProductValues from "../views/ProductValues";
 
 export default function Home() {
   return (
-    <div>
-    <h1>Hello World</h1>
-    <Button variant="contained">Hello world</Button>
-    </div>
+    <>
+     <AppAppBar />
+      <ProductHero />
+      <ProductValues />
+      <ProductCategories />
+      <ProductHowItWorks />
+      <ProductCTA />
+      <ProductSmokingHero />
+      <AppFooter />
+    </>
   )
 }
