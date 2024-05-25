049bf1432a9f2146f5022f4fabd5da4ad2525611
santiago <saforero88@gmail.com>
2024-05-24 20:00:10 -0500

    refactor: Update AppAppBar component to include logo and adjust styling

--- src/views/AppAppBar.tsx
+++ src/views/AppAppBar.tsx
@@ -2,26 +2,19 @@ import Box from "@mui/material/Box";
 import AppBar from "../components/AppBar";
 import Toolbar from "../components/Toolbar";
 
-const rightLink = {
-  fontSize: 16,
-  color: "common.white",
-  ml: 3,
-};
-
 function AppAppBar() {
   return (
-    <div>
+    <>
       <AppBar position="fixed">
         <Toolbar sx={{ justifyContent: "space-between" }}>
+          <img src="/logo-drive-fluecy.svg" alt="logo" style={{ width: "100%", height: "100%", padding: "0.5rem" }} />
           <Box sx={{ flex: 1 }} />
-         
           <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
-           
           </Box>
         </Toolbar>
       </AppBar>
       <Toolbar />
-    </div>
+    </>
   );
 }
 
