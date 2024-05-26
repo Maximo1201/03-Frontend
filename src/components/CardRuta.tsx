ea1bf4c0225b36e9344c867d42617960aef28b43
Luis Molina <luismi_rocks@hotmail.com>
2024-05-26 13:04:20 -0500

    establecer colores, tamanos y comportamiento responsive de mobile y desktop

--- src/components/CardRuta.tsx
+++ src/components/CardRuta.tsx
@@ -1,3 +1,4 @@
+import { Typography, Card } from "@mui/material";
 import Image from "next/image";
 
 interface ICardRutaProps {
@@ -9,11 +10,40 @@ interface ICardRutaProps {
 const CardRuta = ({ icono, titulo, descripcion }: ICardRutaProps) => {
     
   return (
-    <div>
-      <Image src={icono} width={60} height={60} alt="icono" />
-      <h3>{titulo}</h3>
-      <p>{descripcion}</p>
-    </div>
+    <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '0.5rem',
+        width: '12rem',
+        height: '7rem',
+        bgcolor: "#001D3D",
+        boxShadow: '0',
+      }}
+    >
+      <Image src={icono} width={30} height={40} alt="icono" />
+      <Typography
+        variant="h5"
+        color= "#FFC300"
+        fontSize={'0.6rem'}
+        fontWeight={'800'}
+      >
+        {titulo}
+      </Typography>
+      <Typography
+        paragraph
+        color= "#FFFFFF"
+        fontSize={'0.5rem'}
+        fontWeight={'500'}
+        sx={{
+          width: '9.6rem'
+        }}
+      >
+        {descripcion}
+      </Typography>
+    </Card>
   );
 };
 
--- src/pages/ComienzaTuRuta.tsx
+++ src/pages/ComienzaTuRuta.tsx
@@ -1,20 +1,65 @@
 import CardRuta from "@/components/CardRuta";
 import rutas from "@/lib/rutas";
+import { Typography, Box, Divider } from "@mui/material";
 
 const ComienzaTuRuta = () => {
   return (
-    <div>
-      <h2>COMIENZA TU RUTA AL VOLANTE!</h2>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        bgcolor: "#001D3D",
+        paddingBottom: "2rem",
+      }}
+    >
+      <Typography
+        variant="h5"
+        color="#FFC300"
+        fontWeight={600}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "4rem",
+          textShadow: "0px 2px #000000",
+        }}
+      >
+        ¡COMIENZA TU RUTA AL VOLANTE!
+      </Typography>
 
-      {rutas.map((ruta, index) => (
-        <CardRuta
-          key={index}
-          icono={ruta.icono}
-          titulo={ruta.titulo}
-          descripcion={ruta.descripcion}
-        />
-      ))}
-    </div>
+      <Divider
+        sx={{
+          bgcolor: "#FFC300",
+          height: "3px",
+          marginBottom: "1.5rem",
+          width: {
+            xs: "100%",
+            sm: "75%",
+          },
+        }}
+      />
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+          gap: "0.5rem",
+        }}
+      >
+        {rutas.map((ruta, index) => (
+          <CardRuta
+            key={index}
+            icono={ruta.icono}
+            titulo={ruta.titulo}
+            descripcion={ruta.descripcion}
+          />
+        ))}
+      </Box>
+    </Box>
   );
 };
 
